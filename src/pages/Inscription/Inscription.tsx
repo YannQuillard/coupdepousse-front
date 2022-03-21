4cc4f342db25d936f9636058521739cfbb9393ec
Elise <60504990+Passiflore@users.noreply.github.com>
2022-03-21 14:13:13 +0100

    🔥 Starting Inscription Pages

--- src/pages/Inscription/Inscription.tsx
+++ src/pages/Inscription/Inscription.tsx
@@ -4,17 +4,48 @@ import './Inscription.css';
 
 function Inscription() {
   return (
-    <div className="Inscription">
-        <h1 className="title_white">hello passiflore</h1>
-        <div className="bnt_round">
-          <a>Inscription</a>
-        </div>
+    <div className="inscription">
+        <h1 className="title_white">Inscription</h1>
         <div className="input">
+            <input
+              type="text"
+              className="input_content"
+              placeholder="Prénom"
+            />
+            <input
+              type="text"
+              className="input_content"
+              placeholder="Nom"
+            />
+            <input
+              type="text"
+              className="input_content"
+              placeholder="Adresse"
+            />
+            <input
+              type="date"
+              className="input_content"
+              placeholder="Date de naissance"
+            />
+            <input
+              type="text"
+              className="input_content"
+              placeholder="Code postal"
+            />
+            <input
+              type="text"
+              className="input_content"
+              placeholder="Ville"
+            />
             <input
               type="text"
               className="input_content"
               placeholder="Téléphone"
             />
+        </div>
+        
+        <div className="bnt_round">
+          <a>Inscription</a>
         </div>
 
     </div>
