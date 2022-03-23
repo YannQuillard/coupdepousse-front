bd9045d4fc45c18057487b5d9d333db388b86f28
Yann Quillard <y38.quillard@gmail.com>
2022-03-23 11:40:49 +0100

    🛣 Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+
 import './App.css';
 import Inscription from './pages/Inscription/Inscription';
 import Connexion from './pages/Connexion/Connexion';
 import Young_Mission from './pages/Yong_Mission/Young_Mission';
 import Mission_Card from './components/Mission_card/Mission_card';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      {/* <Inscription></Inscription> */}
-      {/* <Connexion></Connexion> */}
-      <Young_Mission></Young_Mission>
-      {/* <Mission_Card></Mission_Card> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={< Home/>} /> 
+          <Route path="/signup" element={< Inscription/>} /> 
+          <Route path="/login" element={< Connexion/>} /> 
+          <Route path="/missions" element={< Young_Mission/>} /> 
+          <Route path="/missioncard" element={< Mission_Card/>} /> 
+        </Routes>
+      </Router>
     </div>
   );
 }
