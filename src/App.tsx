f4d94fa8857a1279244fc9df93e44e5deda16ccc
Elise <60504990+Passiflore@users.noreply.github.com>
2022-03-23 02:45:02 +0100

    Card and Young mission
    Adding card
    Fixing small global error
    adding Young mission

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import Inscription from './pages/Inscription/Inscription';
 import Connexion from './pages/Connexion/Connexion';
+import Young_Mission from './pages/Yong_Mission/Young_Mission';
+import Mission_Card from './components/Mission_card/Mission_card';
 
 function App() {
   return (
     <div className="App">
       {/* <Inscription></Inscription> */}
-      <Connexion></Connexion>
+      {/* <Connexion></Connexion> */}
+      <Young_Mission></Young_Mission>
+      {/* <Mission_Card></Mission_Card> */}
     </div>
   );
 }
---
+++ src/components/Mission_card/Mission_card.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import shopping_cart from "../../assets/images/shopping_cart_icon.svg"
+import './../Mission_card/Mission_card.css';
+
+function Mission_card() {
+
+    const mission_content= {
+        missionTitle: "Faire les courses",
+        missionDate : "29/03/2022 à 14h30",
+        missionDuration : "1 heure"
+
+    }
+    return (
+
+        <div>
+            <div className="mission_card">
+                <div className='mission_card_text small_Text'>
+                    <div className="mission_first_raw">
+                        <img src={shopping_cart}/>
+                        <h4 className='title_green'>{mission_content["missionTitle"]}</h4>
+                    </div>
+                    <div className="mission_second_raw">
+                        <div className='mission_first_column'>
+                            <p>Le: {mission_content["missionDate"]}</p>
+                            <p>Pendant: {mission_content["missionDuration"]}</p>
+                            <a className='bnt_round' href="#">Postuler</a>
+                        </div>
+                        <a className="mission_details" href="#">Détails</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Mission_card;
