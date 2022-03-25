8cb9b1de4798f8c793cb3345ff10707f93ecbdba
Elise <60504990+Passiflore@users.noreply.github.com>
2022-03-25 12:21:54 +0100

    Change Senior Name & Picture

--- src/pages/Young_Landing/Young_Landing.tsx
+++ src/pages/Young_Landing/Young_Landing.tsx
@@ -84,34 +84,42 @@ function Young_Landing() {
 					<Message 
 						TaskContent="Faire ses courses"
 						AvatarUrl="https://img.freepik.com/photos-gratuite/portrait-belle-femme-agee-aux-cheveux-gris-garde-main-bord-lunettes-sourit-joyeusement_273609-50072.jpg"
+						Name="Martine"
+					></Message>
+					<Message 
+						TaskContent="Passer le temps"
+						AvatarUrl="https://www.solimut-mutuelle.fr/sites/default/files/styles/offres_photo_principale/public/portrait_mutuelle_senior.jpg?itok=_xcRPK-m"
+						Name="Jacques"
+					></Message>
+					<Message 
+						TaskContent="Jeux de société"
+						AvatarUrl="https://img.20mn.fr/DmK4sAEqTmuKQl8w-qHzFg/830x532_aider-faire-choix-voici-comparatif-meilleures-tablettes-tactiles-senior.jpg"
+						Name="Robert"
+					></Message>
+					<Message 
+						TaskContent="Petit traveaux"
+						AvatarUrl="https://www.cocoon.fr/wp-content/uploads/2017/03/Pourquoi-shydrater-est-il-crucial-pour-les-personnes-%C3%A2g%C3%A9es_MINI.jpg"
+						Name="Marie"
+					></Message>
+					<Message 
+						TaskContent="Problème Informatique"
+						AvatarUrl="https://thumbs.dreamstime.com/b/senior-homme-en-pull-vert-pouces-vers-le-haut-isol%C3%A9-sur-fond-blanc-214390618.jpg"
+						Name="Louis"
 					></Message>
 					<Message 
 						TaskContent="Faire ses courses"
-						AvatarUrl="https://img.freepik.com/photos-gratuite/portrait-belle-femme-agee-aux-cheveux-gris-garde-main-bord-lunettes-sourit-joyeusement_273609-50072.jpg"
+						AvatarUrl="https://img.freepik.com/photos-gratuite/portrait-90-ans-serieux-bel-homme-senior-portrait-isole-blanc_136401-2168.jpg"
+						Name="Marc"
 					></Message>
 					<Message 
-						TaskContent="Faire ses courses"
-						AvatarUrl="https://img.freepik.com/photos-gratuite/portrait-belle-femme-agee-aux-cheveux-gris-garde-main-bord-lunettes-sourit-joyeusement_273609-50072.jpg"
+						TaskContent="Aller chez le docteur"
+						AvatarUrl="https://www.mannequin-senior.fr/img/portfolio/2021/louis-d.jpg"
+						Name="Jacob"
 					></Message>
 					<Message 
-						TaskContent="Faire ses courses"
-						AvatarUrl="https://img.freepik.com/photos-gratuite/portrait-belle-femme-agee-aux-cheveux-gris-garde-main-bord-lunettes-sourit-joyeusement_273609-50072.jpg"
-					></Message>
-					<Message 
-						TaskContent="Faire ses courses"
-						AvatarUrl="https://img.freepik.com/photos-gratuite/portrait-belle-femme-agee-aux-cheveux-gris-garde-main-bord-lunettes-sourit-joyeusement_273609-50072.jpg"
-					></Message>
-					<Message 
-						TaskContent="Faire ses courses"
-						AvatarUrl="https://img.freepik.com/photos-gratuite/portrait-belle-femme-agee-aux-cheveux-gris-garde-main-bord-lunettes-sourit-joyeusement_273609-50072.jpg"
-					></Message>
-					<Message 
-						TaskContent="Faire ses courses"
-						AvatarUrl="https://img.freepik.com/photos-gratuite/portrait-belle-femme-agee-aux-cheveux-gris-garde-main-bord-lunettes-sourit-joyeusement_273609-50072.jpg"
-					></Message>
-					<Message 
-						TaskContent="Faire ses courses"
-						AvatarUrl="https://img.freepik.com/photos-gratuite/portrait-belle-femme-agee-aux-cheveux-gris-garde-main-bord-lunettes-sourit-joyeusement_273609-50072.jpg"
+						TaskContent="Se promener"
+						AvatarUrl="https://static.actu.fr/uploads/2018/02/mannequin-senior-femme_aletia2011-stock-adobe_-com_-800.jpg"
+						Name="Rosalie"
 					></Message>
 				</div>
 				<div className="activateChat" onClick={changeStyleChat}>+</div>
